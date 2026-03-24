020d6e7242c8cb1171a81065f1c610864019ffd4
kbamgbose <kbamgbose@gmail.com>
2026-03-23 20:57:46 -0700

    Bug fix

--- train.py
+++ train.py
@@ -288,7 +288,7 @@ def get_lr(it):
     if it > max_steps:
         return min_lr
     # 3) in between, use cosine decay down to min learning rate
-    decay_ratio = (it - warmup_steps) / (max_steps = warmup_steps)
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
     assert 0 <= decay_ratio <= 1
     coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff starts at 1 and goes to 0
     return min_lr + coeff * (max_lr - min_lr)f
